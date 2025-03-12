eddeec549af57268094f6dfa965a3cef7feb7057
ArgusMagnus <argusmagnus@outlook.com>
2025-03-12 10:49:32 +0100

    fix

--- Processors/TameableProcessor.cs
+++ Processors/TameableProcessor.cs
@@ -13,12 +13,10 @@ sealed class TameableProcessor(ManualLogSource logger, ModConfig cfg, SharedProc
         if (SharedState.DataRevisions.TryGetValue(zdo.m_uid, out var dataRevision) && dataRevision == zdo.DataRevision)
             return;
 
-        if (!zdo.GetBool(ZDOVars.s_tamed))
-            return;
-
         var fields = zdo.Fields(prefabInfo.Tameable);
-        fields.Set(x => x.m_commandable, Config.Tames.MakeCommandable.Value);
-        if (Config.Tames.SendTamingPogressMessages.Value)
+        if (zdo.GetBool(ZDOVars.s_tamed))
+            fields.Set(x => x.m_commandable, Config.Tames.MakeCommandable.Value);
+        else if (Config.Tames.SendTamingPogressMessages.Value)
         {
             /// <see cref="Tameable.GetRemainingTime()"/>
             var tameTime = fields.GetFloat(x => x.m_tamingTime);
