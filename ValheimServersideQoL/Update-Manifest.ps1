d0b86313db258d8d4edc9e0a336cbb8c9d29d0ca
ArgusMagnus <argusmagnus@outlook.com>
2025-06-29 09:49:14 +0200

    prepare beta release

--- ValheimServersideQoL/Update-Manifest.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)][string]$Path,
-    [Parameter(Mandatory=$true)][string]$VersionNumber,
-    [Parameter(Mandatory=$true)][string]$Version
-)
-
-Set-Content -LiteralPath $Path -Value ((Get-Content -LiteralPath $Path -Raw).Replace('{VersionNumber}',$VersionNumber).Replace('{Version}',$Version))
---
+++ ValheimServersideQoL/Write-Manifest.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)][string]$Path,
+    [Parameter(Mandatory=$true)][string]$VersionNumber,
+    [Parameter(Mandatory=$true)][string]$Version
+)
+
+$manifest = @{
+    name = 'ServersideQoL'
+    version_number = $VersionNumber
+    version = $Version
+    website_url = 'https://github.com/ArgusMagnus/ValheimServersideQoL'
+    description = 'Serverside-only QoL mod, compatible with vanilla (e.g. XBox) clients. Auto-sort chests, track portals/ships on map tables, auto-close doors, refuel smelters from containers, disable rain damage, infinite building/farming stamina and more.'
+    dependencies = @('denikson-BepInExPack_Valheim-5.4.2202')
+}
+
+if ($VersionNumber -ne $Version) {
+    $manifest.description = "This is the public test channel of $($manifest.name)"
+    $manifest.name += '_BETA'
+}
+
+Set-Content -LiteralPath $Path -Value ($manifest | ConvertTo-Json)
