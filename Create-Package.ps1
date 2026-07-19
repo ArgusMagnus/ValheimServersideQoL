6c0f19ac6457566ce6742a1aa8b3dad8cb4da769
ArgusMagnus <argusmagnus@outlook.com>
2026-07-19 15:56:51 +0200

    Automate mod packaging and improve project metadata
    Added Create-Package.ps1 for automated packaging with manifest and dependency handling. Updated build props and targets for versioning, copyright, and packaging. Improved .csproj metadata and dependency management. Included packaging script in solution items.

---
+++ Create-Package.ps1
@@ -0,0 +1,57 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)][string]$Path,
+    [Parameter(Mandatory = $true)][string]$Url,
+    [Parameter(Mandatory = $true)][string]$Description,
+    [Parameter(Mandatory = $false)][string]$Dependencies,
+    [Parameter(Mandatory = $true)][string]$Destination
+)
+
+$ErrorActionPreference = 'Stop'
+
+if ($Description.Length -gt 256) {
+    throw 'Description exceeds 256 characters'
+}
+
+$dir = Split-Path -LiteralPath $Path
+
+$deps = @('denikson-BepInExPack_Valheim-5.4.2333')
+if ($Dependencies) {
+    $deps += $Dependencies.Split(';') | Get-ItemPropertyValue -Name VersionInfo | ForEach-Object {
+        $author = $_.LegalCopyright
+        $name = $_.ProductName
+        $version = $_.ProductVersion.Substring(0, $_.ProductVersion.IndexOfAny(@('-', '+')))
+        "$author-$name-$version"
+    }
+}
+
+$vi = Get-ItemPropertyValue -LiteralPath $Path -Name VersionInfo
+$name = $vi.ProductName
+$version = $vi.ProductVersion.Substring(0, $vi.ProductVersion.IndexOf('+'))
+$versionNumber = $version.Split('-')[0]
+
+# if ($VersionNumber -ne $Version) {
+#     $versionParts = $VersionNumber.Split('.')
+#     $patch = $versionParts[2]
+#     $beta = ($Version -split '-beta.0.')[1].PadLeft(3, '0')
+#     $versionParts[2] = "$patch$beta"
+#     $VersionNumber = $versionParts -join '.'
+# }
+
+$manifest = @{
+    name           = $name
+    version_number = $versionNumber
+    version        = $version
+    website_url    = $Url
+    description    = $Description
+    dependencies   = $deps
+}
+
+# if ($VersionNumber -ne $Version) {
+#     $manifest.description = "This is the public test channel of $($manifest.name)"
+#     $manifest.name += '_BETA'
+# }
+
+Set-Content -LiteralPath "$dir\manifest.json" -Value ($manifest | ConvertTo-Json)
+
+Compress-Archive -Path "$dir\*" -DestinationPath $Destination -Force
