9cedaf6b0b7d0d08bf91db8935b3d1e0ea436234
ArgusMagnus <argusmagnus@outlook.com>
2026-07-20 20:27:57 +0200

    Add auto-pickup and container storage logic
    Expanded ItemDropProcessor to support automatic item pickup and storage into containers. Updated PrefabInfo with new references and added tracking for egg drops and item drops. Implemented logic for player distance checks, container space validation, stacking, ownership handling, inventory updates, and pickup messaging. Introduced ContainerFloats class with PickupRange constant. Added groundwork for future fodder item exclusion.

--- ServersideQoL.AutoStore/AutoStore.cs
+++ ServersideQoL.AutoStore/AutoStore.cs
@@ -7,6 +7,11 @@ namespace ServersideQoL.AutoStore;
 [BepInDependency(ServersideQoL.PluginGuid, ServersideQoL.PluginVersion)]
 partial class AutoStore : ServersideQoLPluginBase<AutoStore, Config>
 {
+    public static class ContainerFloats
+    {
+        public const string PickupRange = "PickupRange";
+    }
+
     protected override Config CreateConfigSingleton(ConfigFile configFile, Logger logger) => new(configFile, logger);
 
     protected override void RegisterProcessors(IProcessorCollection processors)
--- ServersideQoL.AutoStore/ItemDropProcessor.cs
+++ ServersideQoL.AutoStore/ItemDropProcessor.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Configuration;
+﻿using UnityEngine;
+using ServersideQoL.ZDOExtender;
 
 namespace ServersideQoL.AutoStore;
 
@@ -6,18 +7,221 @@ namespace ServersideQoL.AutoStore;
 public sealed class ItemDropProcessor : Processor<ItemDropProcessor.PrefabInfo>
 {
     public const string Id = "5f86a765-e449-4047-afc8-a63e4d681a48";
-    public sealed record PrefabInfo(ItemDrop ItemDrop) : ProcessorPrefabInfo;
+    public sealed record PrefabInfo(ItemDrop ItemDrop, Piece? Piece, EggGrow? EggGrow, ZSyncTransform? ZSyncTransform) : ProcessorPrefabInfo;
 
+    readonly Dictionary<ZDO, DateTimeOffset> _eggDropTime = [];
+    SectorDictionary<HashSet<ZDO>>? _itemDrops;
     SectorDictionary<SharedItemDataKey, HashSet<ZDO>>? _containersByItemName;
 
     protected override void Initialize()
     {
-        _containersByItemName = Instance<ContainerRegistryProcessor>().GetContainersByItemName(1);
+        if (Config.Instance.AutoPickup.Value)
+        {
+            _containersByItemName = Instance<ContainerRegistryProcessor>().GetContainersByItemName(Mathf.Max(Config.Instance.AutoPickupRange.Value, Config.Instance.AutoPickupMaxRange.Value));
+            _itemDrops = new(_containersByItemName.SectorWidth);
+        }
+        else
+        {
+            _containersByItemName = null;
+            _itemDrops = null; 
+        }
+
+        _eggDropTime.Clear();
     }
 
     protected override ProcessResult Process(ZDO zdo, IReadOnlyList<Peer> peers, PrefabInfo prefabInfo)
     {
-        //if (_containersByItemName is null || !Config.Instance.AutoPickup.Value)
-        return ProcessResult.UnregisterProcessor;
+        if (_containersByItemName is null || _itemDrops is null)
+            return ProcessResult.UnregisterProcessor;
+        if (prefabInfo.Piece is not null && zdo.Vars.GetPiece())
+            return ProcessResult.UnregisterProcessor; // ignore placed items (such as feasts)
+
+        if (prefabInfo.EggGrow is not null)
+        {
+            if (zdo.Vars.GetGrowStart() > 0)
+                return ProcessResult.WaitForZDORevisionChange;
+
+            if (!_eggDropTime.TryGetValue(zdo, out var dropTime))
+            {
+                _eggDropTime.Add(zdo, DateTimeOffset.UtcNow);
+                zdo.GetExtension<IExtendedZDO>().Destroyed += x => _eggDropTime.Remove(x);
+                return ProcessResult.Repeat;
+            }
+            if (DateTimeOffset.UtcNow - dropTime < TimeSpan.FromSeconds(2 * prefabInfo.EggGrow.m_updateInterval + 2))
+                return ProcessResult.Repeat;
+        }
+
+        if (!CheckMinDistance(peers, zdo, Config.Instance.AutoPickupMinPlayerDistance.Value))
+            return ProcessResult.Repeat; // player to close
+
+        var shared = prefabInfo.ItemDrop.m_itemData.m_shared;
+        var requestOwn = false;
+        var excludeFodderCheckComplete = !Config.Instance.AutoPickupExcludeFodder.Value;
+        HashSet<Vector2i>? usedSlots = null;
+        List<ZDO>? toRemove = null;
+        ItemDrop.ItemData? item = null;
+
+        foreach (var containers in _containersByItemName.EnumerateAdjacent((zdo.GetPosition(), shared)))
+        {
+            if (containers.Count > 0 && !excludeFodderCheckComplete)
+            {
+                excludeFodderCheckComplete = true;
+                //foreach (var tameState in Instance<TameableProcessor>().Tames)
+                //{
+                //    if (tameState.ZDO.PrefabInfo.Tameable is null)
+                //        continue;
+
+                //    /// <see cref="MonsterAI.CanConsume(ItemDrop.ItemData)"/>
+                //    if (!tameState.ZDO.PrefabInfo.Tameable.Value.MonsterAI.m_consumeItems.Any(x => x.m_itemData.m_shared.m_name == shared.m_name))
+                //        continue;
+                //    var rangeSqr = tameState.ZDO.PrefabInfo.Tameable.Value.MonsterAI.m_consumeSearchRange;
+                //    rangeSqr *= rangeSqr;
+                //    if (Utils.DistanceSqr(zdo.GetPosition(), tameState.ZDO.GetPosition()) < rangeSqr)
+                //    {
+                //        if (prefabInfo.ZSyncTransform is not null && zdo.GetTimeSinceSpawned() < TimeSpan.FromSeconds(10))
+                //            return ProcessResult.Repeat;
+
+                //        ProcessResult result = ProcessResult.UnregisterProcessor;
+                //        var fields = zdo.Fields<ItemDrop>();
+                //        if (fields.UpdateValue(static () => x => x.m_autoPickup, false))
+                //            result = ProcessResult.RecreateZDO;
+                //        if (fields.UpdateValue(static () => x => x.m_autoDestroy, false))
+                //            result = ProcessResult.RecreateZDO;
+                //        return result;
+                //    }
+                //}
+            }
+
+            toRemove?.Clear();
+
+            foreach (var containerZdo in containers)
+            {
+                if (Instance<ContainerRegistryProcessor>().GetState(containerZdo) is not { } containerState)
+                {
+                    (toRemove ??= []).Add(containerZdo);
+                    continue;
+                }
+
+                if (containerZdo.Vars.GetInUse()) // || !CheckMinDistance(peers, containerZdo))
+                    continue; // in use or player to close
+
+                var pickupRangeSqr = containerState.GetFloat(AutoStore.ContainerFloats.PickupRange) ?? Config.Instance.AutoPickupRange.Value;
+                pickupRangeSqr *= pickupRangeSqr;
+
+                if (pickupRangeSqr is 0f || Utils.DistanceSqr(zdo.GetPosition(), containerZdo.GetPosition()) > pickupRangeSqr)
+                    continue;
+
+                if (item is null)
+                {
+                    item = new() { m_shared = shared };
+                    ItemDrop.LoadFromZDO(item, zdo);
+                }
+
+                var stack = item.m_stack;
+                (usedSlots ??= []).Clear();
+
+                var requestContainerOwn = false;
+
+                ItemDrop.ItemData? containerItem = null;
+                foreach (var slot in containerState.InventoryItems)
+                {
+                    usedSlots.Add(slot.m_gridPos);
+                    if (new ItemDataKey(item) != slot)
+                        continue;
+
+                    containerItem ??= slot;
+
+                    var maxAmount = slot.m_shared.m_maxStackSize - slot.m_stack;
+                    if (maxAmount <= 0)
+                        continue;
+
+                    if (Config.Instance.AutoPickupRequestOwnership.Value && !zdo.IsOwnerOrUnassigned())
+                        requestOwn = true;
+                    if (!containerZdo.IsOwnerOrUnassigned())
+                        requestContainerOwn = true;
+                    if (requestOwn || requestContainerOwn)
+                        break;
+
+                    var amount = Math.Min(stack, maxAmount);
+                    slot.m_stack += amount;
+                    stack -= amount;
+                    if (stack is 0)
+                        break;
+                }
+
+                if (containerItem is null)
+                {
+                    (toRemove ??= []).Add(containerZdo);
+                    continue;
+                }
+
+                for (var emptySlots = containerState.Inventory.GetEmptySlots(); stack > 0 && emptySlots > 0; emptySlots--)
+                {
+                    if (Config.Instance.AutoPickupRequestOwnership.Value && !zdo.IsOwnerOrUnassigned())
+                        requestOwn = true;
+                    if (!containerZdo.IsOwnerOrUnassigned())
+                        requestContainerOwn = true;
+                    if (requestOwn || requestContainerOwn)
+                        break;
+
+                    var amount = Math.Min(stack, item.m_shared.m_maxStackSize);
+
+                    var slot = containerItem.Clone();
+                    slot.m_stack = amount;
+                    slot.m_gridPos.x = -1;
+                    for (int x = 0; x < containerState.Inventory.GetWidth() && slot.m_gridPos.x < 0; x++)
+                    {
+                        for (int y = 0; y < containerState.Inventory.GetHeight(); y++)
+                        {
+                            if (usedSlots.Add(new(x, y)))
+                            {
+                                (slot.m_gridPos.x, slot.m_gridPos.y) = (x, y);
+                                break;
+                            }
+                        }
+                    }
+                    containerState.InventoryItems.Add(slot);
+                    stack -= amount;
+                }
+
+                if (requestOwn || requestContainerOwn)
+                {
+                    if (requestContainerOwn)
+                        Instance<ContainerRegistryProcessor>().RequestOwnership(containerZdo, 0);
+                    continue;
+                }
+
+                if (stack != item.m_stack)
+                {
+                    containerState.SaveIntenvory();
+                    (item.m_stack, stack) = (stack, item.m_stack);
+                    ItemDrop.SaveToZDO(item, zdo);
+                    ShowMessage(peers, containerZdo,
+                        Config.Instance.Localization.Value.FormatAutoPickup(containerState.PrefabInfo.Container.m_name, item.m_shared.m_name, stack),
+                        Config.Instance.PickedUpMessageType.Value);
+                }
+
+                if (item.m_stack is 0)
+                    break;
+            }
+
+            if (toRemove is not null)
+            {
+                foreach (var containerZdo in toRemove)
+                    containers.Remove(containerZdo);
+            }
+
+            if (item?.m_stack is 0)
+                return ProcessResult.DestroyZDO;
+
+            if (requestOwn)
+            {
+                RPC.RequestOwn(zdo);
+                return ProcessResult.SkipOtherProcessors | ProcessResult.WaitForZDORevisionChange;
+            }
+        }
+
+        _itemDrops.TryAdd(zdo);
+        return ProcessResult.WaitForZDORevisionChange;
     }
 }
